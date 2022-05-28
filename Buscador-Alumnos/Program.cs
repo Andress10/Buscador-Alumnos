b0183f17b352051b3ae39aeeccadfb6af1fd5ba0
Andress10 <andres_20nuz@hotmail.com>
2022-05-28 14:29:27 -0300

    Agregar archivos de proyecto.

---
+++ Buscador-Alumnos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Buscador_Alumnos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
