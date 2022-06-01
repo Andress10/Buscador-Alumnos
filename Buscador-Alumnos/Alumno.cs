0d0fd25523bdab2331a57383dd19f5ecff73b594
Andress10 <andres_20nuz@hotmail.com>
2022-05-31 22:24:29 -0300

    terminando ejercicios de buscar alumno

---
+++ Buscador-Alumnos/Alumno.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Buscador_Alumnos
+{
+    class Alumno
+    {
+
+        //ATRIBUTOS//
+        private string nombre;
+        private int identificacion;
+        private bool activo;
+        private double promedioNotas;
+        private List<int> notas;
+
+        //-----------------PARTE 1 DEL EJERCICIO-------------//           
+        public Alumno() { }
+
+        public Alumno(string nombre, int identificacion, bool activo, List<int> notas)
+        {
+            this.nombre = nombre;
+            this.identificacion = identificacion;
+            this.activo = true;
+            this.notas = notas;
+        }
+
+        public List<int> GetNotas()
+        {
+            return this.notas;
+        }
+
+        public double GetPromedio() 
+        {
+            double notasSumadas = 0;
+            List<int> notasAPromediar = this.GetNotas();
+            for (int i = 0; i < notasAPromediar.Count; i++)
+            {
+                notasSumadas += notasAPromediar[i];
+            } 
+            return notasSumadas / notasAPromediar.Count;
+        }
+
+        public int GetIdentificacion()
+        {
+            return this.identificacion;
+        }
+
+        public string GetNombre()
+        {
+            return this.nombre;
+        }
+
+        public bool GetActivo()
+        {
+            return this.activo;
+        }
+
+        public void SetterActivo(bool nuevoActivo)
+        {
+            this.activo = nuevoActivo;
+        }
+    }
+}
+
--- Buscador-Alumnos/Program.cs
+++ Buscador-Alumnos/Program.cs
@@ -1,12 +1,67 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Buscador_Alumnos
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Alumno alumno1 = new Alumno("andres", 4, true, new List<int> { 4, 1,-5, 2 });
+            Alumno alumno2 = new Alumno("jose", 8, false, new List<int> { 10, 9, 4, 6 });
+            Alumno alumno3 = new Alumno("andres", 5, true, new List<int> { 10, 5, 4, 6 });
+            Alumno alumno4 = new Alumno("sofia", 20, true, new List<int> { 0, 2, 4, 5 });
+            Alumno alumno5 = new Alumno("andres", 8, false, new List<int> { 10, 9, 4, 6 });
+
+            foreach (int nota in alumno1.GetNotas())
+            {
+                Console.WriteLine("Notas alumno: " + nota);
+            }
+
+            Console.WriteLine($"El promedio del alumno es: {alumno1.GetPromedio()}");
+
+            Console.WriteLine($"La identificacion es: {alumno1.GetIdentificacion()}");
+
+            Console.WriteLine($"La identificacion es: {alumno1.GetNombre()}");
+
+            Console.WriteLine($"El humano activo?: {alumno1.GetActivo()}");
+
+            Console.WriteLine();
+      
+            List<Alumno> alumnos = new List<Alumno> ();
+            alumnos.Add(alumno1);
+            alumnos.Add(alumno2);
+            alumnos.Add(alumno3);
+            alumnos.Add(alumno4);
+            alumnos.Add(alumno5);
+
+            Profesor.RecorrerListaDeAlumnos(Profesor.Nombre("ANDRES", alumnos));
+            Console.WriteLine();
+
+            Profesor.RecorrerAlumno(Profesor.BuscarAlumnoPorIdentificacion(20, alumnos));
+            Console.WriteLine();
+
+            Profesor.RecorrerListaDeAlumnos(Profesor.AlumnosInactivos(alumnos));
+            Console.WriteLine();
+
+            Profesor.RecorrerAlumno(Profesor.AlumnosConMayorPromedioASiete(alumnos));
+            Console.WriteLine();
+
+            Console.WriteLine("Alumnos Desaprobados:");
+            Profesor.RecorrerListaDeAlumnos(Profesor.AlumnosDesaprobado(alumnos));
+
+            Console.WriteLine();
+            Profesor.RecorrerAlumno(Profesor.AlumnoConMayorPromedio(alumnos));
+
+            //EJEMPLO MIO SETTER GETTER
+            Console.WriteLine();
+            Console.WriteLine();
+            alumno1.SetterActivo(false);
+            Console.WriteLine(alumno1.GetActivo());
         }
     }
 }
+
+
+
+
